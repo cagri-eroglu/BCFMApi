0d75dae6b0b38b4aaed283a2ae0a6e715c44b3cb
Cagri <cagrieroglu9@gmail.com>
2024-08-15 17:48:05 -0700

    implementing logic and combining post endpoints because why not ツ

--- Controllers/BCFMController.cs
+++ Controllers/BCFMController.cs
@@ -18,37 +18,70 @@ namespace BCFMApi.Controllers
 		[HttpGet("health")]
 		public IActionResult HealthCheck() => Ok(new { status = "Healthy", timestamp = DateTime.Now });
 
-		[HttpPost("BCFM_post")]
-		public IActionResult PostMethod([FromBody] PostBCFMDto data)
+		//[HttpPost("BCFM_post")]
+		//public IActionResult PostMethod([FromBody] PostBCFMDto data)
+		//{
+		//	if (data == null)
+		//	{
+		//		return BadRequest("Invalid request.");
+		//	}
+
+		//	if (string.IsNullOrEmpty(data.stringBasedProperty))
+		//	{
+		//		return BadRequest("Invalid request.");
+		//	}
+
+		//	return Ok(data);
+		//}
+
+		//[HttpPost("BCFM_post_query")]
+		//public IActionResult PostMessageWithQuery([FromQuery] int? intBasedProperty = null, [FromQuery] string stringBasedProperty = "BC4M ♥")
+		//{
+		//	if (intBasedProperty == null || string.IsNullOrEmpty(stringBasedProperty))
+		//	{
+		//		return BadRequest("Invalid data provided.");
+		//	}
+
+		//	var result = new PostBCFMDto
+		//	{
+		//		intBasedProperty = intBasedProperty.Value,
+		//		stringBasedProperty = stringBasedProperty
+		//	};
+
+		//	return Ok(result);
+		//}
+
+		[HttpPost("BCFMPost")]
+		public IActionResult PostMessageCombined(
+			[FromQuery] int? intBasedProperty = null,
+			[FromQuery] string stringBasedProperty = "BC4M",
+			[FromBody] PostBCFMDto data = null
+	)
 		{
-			if (data == null)
+			if (data != null)
 			{
-				return BadRequest("Invalid request.");
+				if (string.IsNullOrEmpty(stringBasedProperty) && intBasedProperty == null)
+				{
+					return BadRequest("Invalid request. intBasedProp shoudlndt be null");
+				}
+
+				return Ok(data);
 			}
-
-			if (string.IsNullOrEmpty(data.stringBasedProperty))
+			else
 			{
-				return BadRequest("Invalid request.");
+				if (intBasedProperty == null || string.IsNullOrEmpty(stringBasedProperty))
+				{
+					return BadRequest("Invalid request .");
+				}
+
+				var result = new PostBCFMDto
+				{
+					intBasedProperty = intBasedProperty.Value,
+					stringBasedProperty = stringBasedProperty
+				};
+
+				return Ok(result);
 			}
-
-			return Ok(data);
-		}
-
-		[HttpPost("BCFM_post_query")]
-		public IActionResult PostMessageWithQuery([FromQuery] int? intBasedProperty = null, [FromQuery] string stringBasedProperty = "BC4M ♥")
-		{
-			if (intBasedProperty == null || string.IsNullOrEmpty(stringBasedProperty))
-			{
-				return BadRequest("Invalid data provided.");
-			}
-
-			var result = new PostBCFMDto
-			{
-				intBasedProperty = intBasedProperty.Value,
-				stringBasedProperty = stringBasedProperty
-			};
-
-			return Ok(result);
 		}
 
 	}
