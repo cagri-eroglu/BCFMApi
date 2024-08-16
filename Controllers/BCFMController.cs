3afb04c33798782484db650a101fbc2713f407f7
Cagri <cagrieroglu9@gmail.com>
2024-08-15 17:27:43 -0700

    adding controller for endpoint and implement get request

---
+++ Controllers/BCFMController.cs
@@ -0,0 +1,18 @@
+﻿using BCFMApi.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BCFMApi.Controllers
+{
+	public class BCFMController : Controller
+	{
+		[HttpGet("BCFMGet")]
+		public IActionResult GetMessage()
+		{
+			var dto = new GetBCFMDto
+			{
+				Msg = "BC4M"
+			};
+			return Json(dto);
+		}
+	}
+}
